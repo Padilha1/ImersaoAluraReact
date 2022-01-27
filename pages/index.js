b8bca186557d3fd16cfec523ea79426bfdcb3c2f
Matheus Padilha <82119551+Padilha1@users.noreply.github.com>
2022-01-27 16:28:33 -0300

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -3,35 +3,7 @@ import { Box, Button, Text, TextField, Image } from '@skynexui/components';
 import React from 'react';
 import {useRouter} from 'next/router';
 
-function GlobalStyle(){
-    return(
-        <style global jsx>{`
-            *{
-                margin: 0;
-                padding: 0;
-                box-sizing: border-box;
-                background-image: url(../imgs/rosa.jpg);
-                list-syle: none;
-            }
-            body{
-                font-family: 'Open Sans', sans-serif;
-            }
-            /* App fit Height */ 
-            html, body, #__next {
-              min-height: 100vh;
-              display: flex;
-              flex: 1;
-            }
-            #__next {
-              flex: 1;
-            }
-            #__next > * {
-              flex: 1;
-            }
-            /* ./App fit Height */ 
-        `}</style>
-    );
-}
+
 
 function Title(props) {
     const Tag = props.tag || h1;
@@ -72,7 +44,7 @@ export default function PaginaInicial() {
 
     return (
       <>
-        <GlobalStyle />
+        
         <Box
           styleSheet={{
             display: 'flex', alignItems: 'center', justifyContent: 'center',
